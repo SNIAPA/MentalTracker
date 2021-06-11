87453772b234ccc8da77cbcf2ed5cdc047a6210e
Qbibubi <jakub.sobieraj2137@gmail.com>
2021-06-11 16:44:05 +0200

    UI changes
    Changed color palette of the app. Added custom svg icons instead of standard ones.

--- app/src/main/java/com/codeenjoyers/mentaltracker/MainActivity.kt
+++ app/src/main/java/com/codeenjoyers/mentaltracker/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+                R.id.nav_notatki, R.id.nav_stats, R.id.nav_breathing, R.id.nav_settings), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
