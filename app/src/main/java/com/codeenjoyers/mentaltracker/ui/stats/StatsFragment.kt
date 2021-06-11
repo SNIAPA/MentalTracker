81b77ffdc5a94b827cc99e94fc167af2214b3fb4
Qbibubi <jakub.sobieraj2137@gmail.com>
2021-06-11 17:01:34 +0200

    Fixed UI and StatsFragment.kt crash error.

--- app/src/main/java/com/codeenjoyers/mentaltracker/ui/stats/StatsFragment.kt
+++ app/src/main/java/com/codeenjoyers/mentaltracker/ui/stats/StatsFragment.kt
@@ -21,7 +21,7 @@ class StatsFragment : Fragment() {
     ): View? {
         slideshowViewModel =
                 ViewModelProvider(this).get(StatsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+        val root = inflater.inflate(R.layout.fragment_stats, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
         slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
