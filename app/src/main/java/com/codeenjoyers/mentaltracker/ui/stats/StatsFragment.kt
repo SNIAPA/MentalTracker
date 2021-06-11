eabbed82ba196c2793b7747904efa110edfa325d
Nikodem Reszka <61233315+nikodemrx@users.noreply.github.com>
2021-06-11 19:55:56 +0200

    added graph

--- app/src/main/java/com/codeenjoyers/mentaltracker/ui/stats/StatsFragment.kt
+++ app/src/main/java/com/codeenjoyers/mentaltracker/ui/stats/StatsFragment.kt
@@ -1,14 +1,31 @@
 package com.codeenjoyers.mentaltracker.ui.stats
 
+import android.R.attr.data
+import android.content.Context
+import android.graphics.*
+import android.graphics.PorterDuff.*
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.OvalShape
+import android.graphics.drawable.shapes.RectShape
 import android.os.Bundle
+import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.codeenjoyers.mentaltracker.R
+import com.codeenjoyers.mentaltracker.codeclasses.DataPoint
+import com.codeenjoyers.mentaltracker.codeclasses.GraphView
+import java.util.*
+import kotlin.math.absoluteValue
+import java.util.*
+
 
 class StatsFragment : Fragment() {
 
@@ -26,6 +43,15 @@ class StatsFragment : Fragment() {
         slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+        root.findViewById<com.codeenjoyers.mentaltracker.codeclasses.GraphView>(R.id.graph_view).setData(generateRandomDataPoints())
+
+
         return root
+    }
+    private fun generateRandomDataPoints(): List<DataPoint> {
+        val random = Random()
+        return (0..5).map {
+            DataPoint(it, random.nextInt(50) + 1)
+        }
     }
 }
