0d41d2d7e1b9e2f27dc084604b05b8e723eddb1b
Mateusz Mazurek <mauzer.official.1@gmail.com>
2021-06-11 16:56:09 +0200

    slidershow

--- app/src/main/java/com/codeenjoyers/mentaltracker/MainActivity.kt
+++ app/src/main/java/com/codeenjoyers/mentaltracker/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.codeenjoyers.mentaltracker
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -13,6 +15,7 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.codeenjoyers.mentaltracker.ui.slideshow.SlideshowFragment
 
 class MainActivity : AppCompatActivity() {
 
