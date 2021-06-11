e79bb70e7d9c0a3b513622c236401577e5d37070
Nikodem Reszka <61233315+nikodemrx@users.noreply.github.com>
2021-06-11 16:09:50 +0200

    added stats option

---
+++ app/src/main/java/com/codeenjoyers/mentaltracker/ui/stats/StatsFragment.kt
@@ -0,0 +1,31 @@
+package com.codeenjoyers.mentaltracker.ui.stats
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.codeenjoyers.mentaltracker.R
+
+class StatsFragment : Fragment() {
+
+    private lateinit var slideshowViewModel: StatsViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        slideshowViewModel =
+                ViewModelProvider(this).get(StatsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+        val textView: TextView = root.findViewById(R.id.text_slideshow)
+        slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
---
+++ app/src/main/java/com/codeenjoyers/mentaltracker/ui/stats/StatsViewModel.kt
@@ -0,0 +1,13 @@
+package com.codeenjoyers.mentaltracker.ui.stats
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class StatsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is stats Fragment"
+    }
+    val text: LiveData<String> = _text
+}
