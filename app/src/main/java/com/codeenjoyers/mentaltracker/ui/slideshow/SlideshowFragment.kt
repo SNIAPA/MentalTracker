70db58e0c985f87a0efccf582d14f70b4a992403
Mateusz Mazurek <mauzer.official.1@gmail.com>
2021-06-11 17:29:19 +0200

    slidershow

--- app/src/main/java/com/codeenjoyers/mentaltracker/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/codeenjoyers/mentaltracker/ui/slideshow/SlideshowFragment.kt
@@ -1,10 +1,13 @@
 package com.codeenjoyers.mentaltracker.ui.slideshow
 
 import android.content.Context
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
@@ -16,15 +19,27 @@ class SlideshowFragment : Fragment() {
 
     private lateinit var imageView: ImageView
     private lateinit var seekBar: SeekBar
+    private lateinit var next: Button
+    private lateinit var submit: Button
+    private lateinit var back: Button
+    private var page : Int = 0
+    private var selectionList: MutableList<String> = mutableListOf()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
 
         imageView = root.findViewById(R.id.imageView2)
         seekBar = root.findViewById(R.id.seekBar)
+        back = root.findViewById(R.id.back)
+        submit = root.findViewById(R.id.submit)
+        next = root.findViewById(R.id.next)
+
+        createPage(page)
 
         seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
             override fun onStopTrackingTouch(seekBar: SeekBar) {
@@ -40,6 +55,37 @@ class SlideshowFragment : Fragment() {
             }
         })
 
+        next.setOnClickListener {
+            //selectionList.add(getSelectionValue())
+            createPage(++page)
+        }
+
+        back.setOnClickListener {
+            selectionList.removeLast()
+            createPage(--page)
+        }
+
+        submit.setOnClickListener {
+        }
+
         return root
+    }
+
+    private fun createPage(index: Int) : Unit {
+        when (index) {
+            0 -> {
+                imageView.background = (Drawable.createFromPath("@drawable/image"))
+                back.isEnabled = false
+                next.isEnabled = true
+            }
+            1 -> {
+                back.isEnabled = true
+                next.isEnabled = true
+            }
+            2 -> {
+                back.isEnabled = true
+                next.isEnabled = false
+            }
+        }
     }
 }
