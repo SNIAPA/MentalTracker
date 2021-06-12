4b159ab67db01319a49b23beb08366cb5652a40b
Mateusz Mazurek <mauzer.official.1@gmail.com>
2021-06-12 06:27:09 +0200

    data saving & loading | update

--- app/src/main/java/com/codeenjoyers/mentaltracker/ui/home/HomeFragment.kt
+++ app/src/main/java/com/codeenjoyers/mentaltracker/ui/home/HomeFragment.kt
@@ -110,9 +110,6 @@ class HomeFragment : Fragment() {
             myAdapter.updateRecords(newRecords)
 
         }
-
-
-
         ListViews.adapter = myAdapter
         return root
     }
@@ -155,7 +152,7 @@ class HomeFragment : Fragment() {
             val textValue2 = row.findViewById<TextView>(R.id.textValue2)
             val date = row.findViewById<TextView>(R.id.date)
 
-            if((mRecords[position].mCustom != "none") || (mRecords[position].mCustom != "null")){
+            if((mRecords[position].mCustom != "none") && (mRecords[position].mCustom != "null")){
                 textValue.text = mRecords[position].mCustom
             }else {
                 textValue.text = "No notes"
