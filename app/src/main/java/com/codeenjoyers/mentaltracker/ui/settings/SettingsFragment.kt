d64e3af9c9006fd0db5670bfbb80f93034a6532f
Mateusz Mazurek <mauzer.official.1@gmail.com>
2021-06-12 06:33:09 +0200

    data saving & loading | update

--- app/src/main/java/com/codeenjoyers/mentaltracker/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/codeenjoyers/mentaltracker/ui/settings/SettingsFragment.kt
@@ -16,20 +16,13 @@ import java.io.FileOutputStream
 
 class SettingsFragment : Fragment() {
 
-    private lateinit var settingsViewModel: SettingsViewModel
-
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        settingsViewModel =
-                ViewModelProvider(this).get(SettingsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
         val textView: TextView = root.findViewById(R.id.text_gallery)
-        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         val SAVEFILE = File(context?.filesDir, SlideshowFragment.FILENAME)
         root.findViewById<Button>(R.id.resetBtn).setOnClickListener {
             FileOutputStream(SAVEFILE).write(
