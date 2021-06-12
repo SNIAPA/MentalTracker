d2d3103d3ceec5559e226e820830f3851c734989
Nikodem Reszka <61233315+nikodemrx@users.noreply.github.com>
2021-06-12 06:45:17 +0200

    bugfixes

--- app/src/main/java/com/codeenjoyers/mentaltracker/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/codeenjoyers/mentaltracker/ui/gallery/GalleryFragment.kt
@@ -14,6 +14,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.codeenjoyers.mentaltracker.R
 import com.codeenjoyers.mentaltracker.ui.slideshow.MoodDataClass
+import java.lang.Exception
 
 class GalleryFragment : Fragment() {
 
@@ -37,42 +38,43 @@ class GalleryFragment : Fragment() {
 
 
         root.findViewById<Button>(R.id.breathBtn).setOnClickListener {
-            root.findViewById<Button>(R.id.breathBtn).isEnabled=false
-            root.findViewById<Button>(R.id.breathBtn).startAnimation(AnimationUtils.loadAnimation(context,R.anim.fade_out))
-            root.findViewById<TextView>(R.id.text_gallery).startAnimation(AnimationUtils.loadAnimation(context,R.anim.fade_out))
+            try{root.findViewById<Button>(R.id.breathBtn).isEnabled=false
+                root.findViewById<Button>(R.id.breathBtn).startAnimation(AnimationUtils.loadAnimation(context,R.anim.fade_out))
+                root.findViewById<TextView>(R.id.text_gallery).startAnimation(AnimationUtils.loadAnimation(context,R.anim.fade_out))}catch(e:Exception){}
 
-            Handler().postDelayed({
-                root.findViewById<TextView>(R.id.text_gallery).alpha=1F
+
+            Handler().postDelayed({try{root.findViewById<TextView>(R.id.text_gallery).alpha=1F
                 root.findViewById<TextView>(R.id.text_gallery).startAnimation(AnimationUtils.loadAnimation(context,R.anim.blink))
-                root.findViewById<Button>(R.id.breathBtn).alpha=0F
+                root.findViewById<Button>(R.id.breathBtn).alpha=0F}catch(e:Exception){}
+
             }, 1000)
 
-            Handler().postDelayed({
-                root.findViewById<TextView>(R.id.text_gallery).text="2"
-                root.findViewById<TextView>(R.id.text_gallery).startAnimation(AnimationUtils.loadAnimation(context,R.anim.blink))
+            Handler().postDelayed({try{root.findViewById<TextView>(R.id.text_gallery).text="2"
+                root.findViewById<TextView>(R.id.text_gallery).startAnimation(AnimationUtils.loadAnimation(context,R.anim.blink))}catch(e:Exception){}
+
             }, 3000)
 
-            Handler().postDelayed({
-                root.findViewById<TextView>(R.id.text_gallery).text="1"
-                root.findViewById<TextView>(R.id.text_gallery).startAnimation(AnimationUtils.loadAnimation(context,R.anim.blink))
+            Handler().postDelayed({try{                root.findViewById<TextView>(R.id.text_gallery).text="1"
+                root.findViewById<TextView>(R.id.text_gallery).startAnimation(AnimationUtils.loadAnimation(context,R.anim.blink))}catch(e:Exception){}
+
             }, 5000)
-            Handler().postDelayed({
-                root.findViewById<TextView>(R.id.text_gallery).alpha=0F
-                root.findViewById<TextView>(R.id.text_gallery).text="3"
+            Handler().postDelayed({try{                root.findViewById<TextView>(R.id.text_gallery).alpha=0F
+                root.findViewById<TextView>(R.id.text_gallery).text="3"}catch(e:Exception){}
+
             }, 7000)
 
-            Handler().postDelayed({ root.findViewById<ImageView>(R.id.imageView3).startAnimation(AnimationUtils.loadAnimation(context,R.anim.first_zoom_in)) }, 7000)
-            Handler().postDelayed({ root.findViewById<ImageView>(R.id.imageView3).startAnimation(AnimationUtils.loadAnimation(context,R.anim.zoom_out)) }, 10000)
-            Handler().postDelayed({ root.findViewById<ImageView>(R.id.imageView3).startAnimation(AnimationUtils.loadAnimation(context,R.anim.zoom_in)) }, 13000)
-            Handler().postDelayed({ root.findViewById<ImageView>(R.id.imageView3).startAnimation(AnimationUtils.loadAnimation(context,R.anim.zoom_out)) }, 16000)
-            Handler().postDelayed({ root.findViewById<ImageView>(R.id.imageView3).startAnimation(AnimationUtils.loadAnimation(context,R.anim.zoom_in)) }, 19000)
-            Handler().postDelayed({ root.findViewById<ImageView>(R.id.imageView3).startAnimation(AnimationUtils.loadAnimation(context,R.anim.zoom_out)) }, 22000)
-            Handler().postDelayed({ root.findViewById<ImageView>(R.id.imageView3).startAnimation(AnimationUtils.loadAnimation(context,R.anim.last_zoom_out)) }, 25000)
-            Handler().postDelayed({ root.findViewById<Button>(R.id.breathBtn).startAnimation(AnimationUtils.loadAnimation(context,R.anim.fade_in)) }, 28000)
+            Handler().postDelayed({try{ root.findViewById<ImageView>(R.id.imageView3).startAnimation(AnimationUtils.loadAnimation(context,R.anim.first_zoom_in))}catch(e:Exception){} }, 7000)
+            Handler().postDelayed({try{root.findViewById<ImageView>(R.id.imageView3).startAnimation(AnimationUtils.loadAnimation(context,R.anim.zoom_out))}catch(e:Exception){}  }, 10000)
+            Handler().postDelayed({try{root.findViewById<ImageView>(R.id.imageView3).startAnimation(AnimationUtils.loadAnimation(context,R.anim.zoom_in))}catch(e:Exception){}  }, 13000)
+            Handler().postDelayed({try{root.findViewById<ImageView>(R.id.imageView3).startAnimation(AnimationUtils.loadAnimation(context,R.anim.zoom_out))}catch(e:Exception){}  }, 16000)
+            Handler().postDelayed({try{root.findViewById<ImageView>(R.id.imageView3).startAnimation(AnimationUtils.loadAnimation(context,R.anim.zoom_in))}catch(e:Exception){}  }, 19000)
+            Handler().postDelayed({try{root.findViewById<ImageView>(R.id.imageView3).startAnimation(AnimationUtils.loadAnimation(context,R.anim.zoom_out))}catch(e:Exception){}  }, 22000)
+            Handler().postDelayed({try{root.findViewById<ImageView>(R.id.imageView3).startAnimation(AnimationUtils.loadAnimation(context,R.anim.last_zoom_out))}catch(e:Exception){}  }, 25000)
+            Handler().postDelayed({try{root.findViewById<Button>(R.id.breathBtn).startAnimation(AnimationUtils.loadAnimation(context,R.anim.fade_in))}catch(e:Exception){}  }, 28000)
 
-            Handler().postDelayed({
-                root.findViewById<Button>(R.id.breathBtn).alpha=1F
-                root.findViewById<Button>(R.id.breathBtn).isEnabled=true
+            Handler().postDelayed({try{                root.findViewById<Button>(R.id.breathBtn).alpha=1F
+                root.findViewById<Button>(R.id.breathBtn).isEnabled=true}catch(e:Exception){}
+
             }, 29000)
 
 
