274c860598e5112a68a0bf965b9d2714ae951513
Nikodem Reszka <61233315+nikodemrx@users.noreply.github.com>
2021-06-12 03:38:41 +0200

    added breathing excercise

--- app/src/main/java/com/codeenjoyers/mentaltracker/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/codeenjoyers/mentaltracker/ui/gallery/GalleryFragment.kt
@@ -1,14 +1,19 @@
 package com.codeenjoyers.mentaltracker.ui.gallery
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.codeenjoyers.mentaltracker.R
+import org.w3c.dom.Text
 
 class GalleryFragment : Fragment() {
 
@@ -26,6 +31,52 @@ class GalleryFragment : Fragment() {
         galleryViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+        root.findViewById<TextView>(R.id.text_gallery).isEnabled=false
+        root.findViewById<TextView>(R.id.text_gallery).alpha=0.0F
+
+
+        root.findViewById<Button>(R.id.breathBtn).setOnClickListener {
+            root.findViewById<Button>(R.id.breathBtn).isEnabled=false
+            root.findViewById<Button>(R.id.breathBtn).startAnimation(AnimationUtils.loadAnimation(context,R.anim.fade_out))
+            root.findViewById<TextView>(R.id.text_gallery).startAnimation(AnimationUtils.loadAnimation(context,R.anim.fade_out))
+
+            Handler().postDelayed({
+                root.findViewById<TextView>(R.id.text_gallery).alpha=1F
+                root.findViewById<TextView>(R.id.text_gallery).startAnimation(AnimationUtils.loadAnimation(context,R.anim.blink))
+                root.findViewById<Button>(R.id.breathBtn).alpha=0F
+            }, 1000)
+
+            Handler().postDelayed({
+                root.findViewById<TextView>(R.id.text_gallery).text="2"
+                root.findViewById<TextView>(R.id.text_gallery).startAnimation(AnimationUtils.loadAnimation(context,R.anim.blink))
+            }, 3000)
+
+            Handler().postDelayed({
+                root.findViewById<TextView>(R.id.text_gallery).text="1"
+                root.findViewById<TextView>(R.id.text_gallery).startAnimation(AnimationUtils.loadAnimation(context,R.anim.blink))
+            }, 5000)
+            Handler().postDelayed({
+                root.findViewById<TextView>(R.id.text_gallery).alpha=0F
+            }, 7000)
+
+            Handler().postDelayed({ root.findViewById<ImageView>(R.id.imageView3).startAnimation(AnimationUtils.loadAnimation(context,R.anim.first_zoom_in)) }, 8000)
+            Handler().postDelayed({ root.findViewById<ImageView>(R.id.imageView3).startAnimation(AnimationUtils.loadAnimation(context,R.anim.zoom_out)) }, 100000)
+            Handler().postDelayed({ root.findViewById<ImageView>(R.id.imageView3).startAnimation(AnimationUtils.loadAnimation(context,R.anim.zoom_in)) }, 12000)
+            Handler().postDelayed({ root.findViewById<ImageView>(R.id.imageView3).startAnimation(AnimationUtils.loadAnimation(context,R.anim.zoom_out)) }, 14000)
+            Handler().postDelayed({ root.findViewById<ImageView>(R.id.imageView3).startAnimation(AnimationUtils.loadAnimation(context,R.anim.last_zoom_out)) }, 16000)
+            Handler().postDelayed({ root.findViewById<Button>(R.id.breathBtn).startAnimation(AnimationUtils.loadAnimation(context,R.anim.fade_in)) }, 180000)
+
+            Handler().postDelayed({
+                root.findViewById<Button>(R.id.breathBtn).alpha=1F
+                root.findViewById<Button>(R.id.breathBtn).isEnabled=true
+            }, 140000)
+
+
+
+        }
         return root
+
+
     }
+
 }
--- app/src/main/java/com/codeenjoyers/mentaltracker/ui/stats/StatsFragment.kt
+++ app/src/main/java/com/codeenjoyers/mentaltracker/ui/stats/StatsFragment.kt
@@ -1,15 +1,29 @@
 package com.codeenjoyers.mentaltracker.ui.stats
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.codeenjoyers.mentaltracker.R
+import com.codeenjoyers.mentaltracker.codeclasses.DataPoint
+import com.codeenjoyers.mentaltracker.ui.slideshow.SlideshowFragment
+import java.io.File
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
+
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class StatsFragment : Fragment() {
 
     private lateinit var slideshowViewModel: StatsViewModel
@@ -26,6 +40,128 @@ class StatsFragment : Fragment() {
         slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+        generateDataPoints()
+        root.findViewById<com.codeenjoyers.mentaltracker.codeclasses.GraphView>(R.id.graph_view).setData(generateRandomDataPoints(6))
+        root.findViewById<SeekBar>(R.id.seekBar2).setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, currentValue: Int, p2: Boolean) {
+                if (currentValue == 0 ){
+                    root.findViewById<com.codeenjoyers.mentaltracker.codeclasses.GraphView>(R.id.graph_view).setData(generateRandomDataPoints(6))
+                }
+                if (currentValue == 1 ){
+                    root.findViewById<com.codeenjoyers.mentaltracker.codeclasses.GraphView>(R.id.graph_view).setData(generateRandomDataPoints(4))
+                }
+                if (currentValue == 2 ){
+                    root.findViewById<com.codeenjoyers.mentaltracker.codeclasses.GraphView>(R.id.graph_view).setData(generateRandomDataPoints(12))
+                }
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+
+        })
+
         return root
+    }
+
+
+
+    private fun generateRandomDataPoints(dataNum:Int): List<DataPoint> {
+        val random = Random()
+        return (0 until dataNum).map {
+            DataPoint(it, random.nextInt(10))
+            //DataPoint(it, 1)
+        }
+    }
+    @SuppressLint("SimpleDateFormat")
+    private fun generateDataPoints(): List<DataPoint> {
+        val random = Random()
+        val SAVEFILE = File(context?.filesDir, SlideshowFragment.FILENAME)
+        var fileString: String
+        if (SAVEFILE.exists()){
+
+            fileString = requireContext().openFileInput(SlideshowFragment.FILENAME)!!.bufferedReader().useLines { lines ->
+                lines.fold("") { some, text ->
+                    "$some$text"
+                }
+            }
+
+        }else{
+            //fileString = ""
+            fileString = "0000:00:00:11:12/Happy/none&0000:00:00:11:12/Happy/none&0000:00:00:11:12/Happy/none&0000:00:00:11:12/Happy/none&0000:00:00:11:12/Happy/none&0000:00:00:11:12/Happy/none&0000:00:00:11:12/Happy/none&0000:00:00:11:12/Happy/none&0000:00:00:11:12/Happy/none&0000:00:00:11:12/Happy/none&"
+            saveData(fileString)
+        }
+
+        var splitedsting = fileString.split("&")
+        var dates = mutableListOf<Date>()
+        for(record in splitedsting){
+            if (record == "") {
+                continue
+            }
+            var recordsplited = record.split("/")
+            var splittedData = recordsplited[0].split(":")
+            //Log.d("Test", splittedData.toString()+" "+record)
+            if(record!="/null"){
+                var newData = splittedData[0] +"-"+splittedData[1]+"-"+splittedData[2]+" "+splittedData[3]+"-"+splittedData[4]
+                val parser = SimpleDateFormat("yyyy-MM-dd HH-mm")
+                val output = parser.parse(newData)
+                dates.add(output)
+            }
+        }
+
+        dates.reverse()
+        var DataPointGraph= mutableListOf<DataPoint>()
+        var lastWeek= mutableListOf<Date>()
+        var lastDate = dates[0]
+        val parser = SimpleDateFormat("yyyy-MM-dd HH-mm")
+        for (date in dates){
+            if(date.after(Date(lastDate.time-(86400000*7)))){
+                lastWeek.add(date)
+            }
+        }
+        lastWeek.sortBy { Ddate ->
+            Ddate.day
+        }
+        var lastDaydata = -1
+        var newDays = 0
+        lastWeek.forEachIndexed{i,date->
+                if (lastDaydata!=date.day){
+                    newDays+=1
+                    lastDaydata=date.day
+                    DataPointGraph.add(DataPoint(newDays, 1))
+                }
+                else{
+                    DataPointGraph[newDays-1].yVal+=1
+                }
+
+        }
+
+        return DataPointGraph
+    }
+
+
+    fun saveData(data : String = null?:"null", note : String = null?:"null") : Unit{
+        fun format(string: String) : String {
+            fun convertTime(lastTimeUsed: Long): String {
+                val date = Date(lastTimeUsed)
+                val format = SimpleDateFormat("yyyy:MM:dd:HH:mm", Locale.ROOT)
+
+                return format.format(date)
+            }
+
+            return (convertTime( System.currentTimeMillis() ) + "/" + string + "/" + note)
+        }
+
+        val SAVEFILE = File(context?.filesDir, SlideshowFragment.FILENAME)
+        val oldText = if (SAVEFILE.exists()) requireContext().openFileInput(SlideshowFragment.FILENAME).bufferedReader().useLines { lines ->
+            lines.fold("") { some, text ->
+                "$some$text"
+            }}.toString() else ""
+
+        val newText = (oldText+format(data) + SlideshowFragment.INTERSPACE)
+
+        FileOutputStream(SAVEFILE).write(
+            newText.toByteArray()
+        )
     }
 }
